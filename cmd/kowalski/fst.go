6d0d21c86f9c78aaf0905962d5c5c6bcb8762282
Chris Smith <chris@chameth.com>
2022-11-25 12:13:25 +0000

    Add command for finding link words
    e.g. `!link log fever` => cabin (log cabin, cabin fever)

--- cmd/kowalski/fst.go
+++ cmd/kowalski/fst.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	regexp2 "regexp"
 	"sort"
 	"strings"
 
@@ -71,33 +72,124 @@ func init() {
 	}
 }
 
-func fstQuery(automaton vellum.Automaton, input string, r Replier) {
-	iterator, err := transducer.Search(automaton, nil, nil)
-	if err != nil {
-		if errors.Is(err, vellum.ErrIteratorDone) {
-			r.reply("No results found")
-			return
-		}
+func WordLink(input string, r Replier) {
+	parts := strings.Split(input, " ")
+	if len(parts) != 2 {
+		r.reply("Invalid input, must specify two words to link")
+		return
+	}
 
+	leftAut, err := regexp.New(fmt.Sprintf("%s[ -]?([a-zA-Z]{3,})", regexp2.QuoteMeta(parts[0])))
+	if err != nil {
+		r.reply("Error: %s", err.Error())
+		return
+	}
+	leftSide, err := fstResults(leftAut)
+	if err != nil {
+		r.reply("Error: %s", err.Error())
+		return
+	} else if len(leftSide) == 0 {
+		r.reply("No words found that can follow '%s'", parts[0])
+		return
+	}
+
+	rightAut, err := regexp.New(fmt.Sprintf("([a-zA-Z]{3,})[ -]?%s", regexp2.QuoteMeta(parts[1])))
+	if err != nil {
 		r.reply("Error: %s", err.Error())
 		return
 	}
 
-	type Match struct {
-		Term  string
-		Score uint64
+	rightSide, err := fstResults(rightAut)
+	if err != nil {
+		r.reply("Error: %s", err.Error())
+		return
+	} else if len(leftSide) == 0 {
+		r.reply("No words found that can precede '%s'", parts[1])
+		return
 	}
 
-	var matches []Match
-	for err == nil {
-		key, val := iterator.Current()
-		matches = append(matches, Match{Term: string(key), Score: val})
-		err = iterator.Next()
+	var leftScores = make(map[string]uint64)
+	for i := range leftSide {
+		leftTerm := strings.TrimSpace(strings.TrimPrefix(leftSide[i].Term, parts[0]))
+		if normal, _, _ := transducer.Get([]byte(leftTerm)); normal < 5_000_000 {
+			leftScores[leftTerm] += leftSide[i].Score
+		}
 	}
 
-	sort.Slice(matches, func(i, j int) bool {
-		return matches[i].Score > matches[j].Score
+	var rightScores = make(map[string]uint64)
+	for i := range rightSide {
+		rightTerm := strings.TrimSpace(strings.TrimSuffix(rightSide[i].Term, parts[1]))
+		if normal, _, _ := transducer.Get([]byte(rightTerm)); normal < 5_000_000 {
+			rightScores[rightTerm] += rightSide[i].Score
+		}
+	}
+
+	var combined []fstMatch
+	for rightTerm := range rightScores {
+		if leftScore, ok := leftScores[rightTerm]; ok {
+			var score uint64
+			if leftScore < rightScores[rightTerm] {
+				score = leftScore
+			} else {
+				score = rightScores[rightTerm]
+			}
+
+			if score > 2 {
+				combined = append(combined, fstMatch{
+					Term:  rightTerm,
+					Score: score,
+				})
+			}
+		}
+	}
+
+	if len(combined) == 0 {
+		r.reply("No linking words found")
+		return
+	}
+
+	sort.Slice(combined, func(i, j int) bool {
+		return combined[i].Score > combined[j].Score
 	})
+
+	message := strings.Builder{}
+	message.WriteString("Linking words for '")
+	message.WriteString(parts[0])
+	message.WriteString("' <> '")
+	message.WriteString(parts[1])
+	message.WriteString("': ")
+
+	for i := range combined {
+		message.WriteString(fmt.Sprintf("`%s` (%d) ", combined[i].Term, combined[i].Score))
+		if message.Len() > 1900 {
+			message.WriteString("[...]")
+			break
+		}
+	}
+
+	r.reply("%s", message.String())
+}
+
+func init() {
+	if *fstModel != "" {
+		addCommand(textCommands, WordLink, "Attempts to find a word that links two others", "wordlink", "link")
+	}
+}
+
+type fstMatch struct {
+	Term  string
+	Score uint64
+}
+
+func fstQuery(automaton vellum.Automaton, input string, r Replier) {
+	matches, err := fstResults(automaton)
+	if err != nil {
+		r.reply("Error: %s", err.Error())
+		return
+	} else if len(matches) == 0 {
+		r.reply("No results found")
+		return
+	}
 
 	message := strings.Builder{}
 	message.WriteString("Matches for '")
@@ -113,4 +205,28 @@ func fstQuery(automaton vellum.Automaton, input string, r Replier) {
 	}
 
 	r.reply("%s", message.String())
+}
+
+func fstResults(automaton vellum.Automaton) ([]fstMatch, error) {
+	iterator, err := transducer.Search(automaton, nil, nil)
+	if err != nil {
+		if errors.Is(err, vellum.ErrIteratorDone) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	var matches []fstMatch
+	for err == nil {
+		key, val := iterator.Current()
+		matches = append(matches, fstMatch{Term: string(key), Score: val})
+		err = iterator.Next()
+	}
+
+	sort.Slice(matches, func(i, j int) bool {
+		return matches[i].Score > matches[j].Score
+	})
+
+	return matches, nil
 }
